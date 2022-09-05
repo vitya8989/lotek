b894827902e6664c2b25dad12cf59432c7773f25
Victor <vitya898989@gmail.com>
2022-09-05 13:14:43 +0300

    fsdfsd

--- src/components/lk/applications_forms/applications_forms.js
+++ src/components/lk/applications_forms/applications_forms.js
@@ -3,10 +3,18 @@ const applicationFormObjectInfo = document.querySelector('.application_form__obj
 const jsApplicationFormBlock = document.querySelector('.js_application_form_block');
 if (applicationFormObjectInfo) {
     const applicationFormObjectInfoEmpty = applicationFormObjectInfo.cloneNode(true);
+    applicationFormObjectInfoEmpty.classList.add('this--mt_30');
+    const applicationFormObjectInfoEmptyCross = document.createElement('span');
+    applicationFormObjectInfoEmptyCross.classList.add('application_form__object_info_cross');
+    applicationFormObjectInfoEmpty.append(applicationFormObjectInfoEmptyCross);
 
     applicationFormObjectInfoBtn.onclick = (e) => {
         e.preventDefault();
         let newObjectInfo = applicationFormObjectInfoEmpty.cloneNode(true);
         jsApplicationFormBlock.insertBefore(newObjectInfo, applicationFormObjectInfoBtn);
+        let newObjectInfoCross = newObjectInfo.querySelector('.application_form__object_info_cross');
+        newObjectInfoCross.onclick = () => {
+            newObjectInfo.remove();
+        }
     }
 }
